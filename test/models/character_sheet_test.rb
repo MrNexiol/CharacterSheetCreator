217b3930efa8b56b976c1b161fc765281740dc84
Tomasz Kopycinski <mrnexiol@gmail.com>
2020-08-23 20:37:41 +0200

    Added buttons allowing user to change stat values by javascript

--- test/models/character_sheet_test.rb
+++ test/models/character_sheet_test.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: character_sheets
+#
+#  id            :integer          not null, primary key
+#  name          :string
+#  experience    :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  user_id       :integer
+#  brawn         :integer          default(10)
+#  intelligence  :integer          default(10)
+#  perception    :integer          default(10)
+#  agility       :integer          default(10)
+#  dexterity     :integer          default(10)
+#  determination :integer          default(10)
+#  barter        :float            default(0.0)
+#  money         :float            default(0.0)
+#  description   :string
+#
 require 'test_helper'
 
 class CharacterSheetTest < ActiveSupport::TestCase
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string
+#  email           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string
+#
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
