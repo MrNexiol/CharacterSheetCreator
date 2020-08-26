a0fe61f6bbe43f6d70844030353fc1360b35d9fd
Tomasz Kopycinski <mrnexiol@gmail.com>
2020-08-26 18:38:01 +0200

    Completely refactored the way stats calculate their value and exp cost

--- app/javascript/controllers/new_character_sheet_controller.js
+++ app/javascript/controllers/new_character_sheet_controller.js
@@ -1,94 +1,76 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ['exp', 'brawn', 'deter', 'intel', 'perce', 'dexte', 'agili', 'plus', 'minus']
+  static targets = ['exp', 'stat', 'plus', 'minus']
 
-  addExp(target){
-    const expPoints = parseInt(this.expTarget.value);
-    switch (parseInt(target.value)) {
+  connect(){
+    this.checkStatLevels()
+  }
+
+  checkStatLevels(){
+    for (let i = 0; i < this.statTargets.length; i++){
+      const canUpgrade = this.isStatUpgradeable(parseInt(this.statTargets[i].value));
+      this.plusTargets[i].disabled = !canUpgrade;
+    }
+  }
+
+  isStatUpgradeable(testValue){
+    const expPoints = this.expTarget.value
+    switch (testValue) {
+      case 20:
+        return false
       case 19:
-        this.expTarget.value = expPoints + 20
-        break
+        return (expPoints - 20) >= 0;
       case 18:
       case 17:
       case 16:
-        this.expTarget.value = expPoints + 15
-        break
+        return (expPoints - 15) >= 0;
       case 15:
       case 14:
       case 13:
-        this.expTarget.value = expPoints + 10
-        break
+        return (expPoints - 10) >= 0;
       default:
-        this.expTarget.value = expPoints + 5
-        break
+        return (expPoints - 5) >= 0;
     }
   }
 
-  cutExp(target){
-    const expPoints = parseInt(this.expTarget.value);
-    switch (parseInt(target.value)) {
+  changeStat(e){
+    const buttonId = e.target.dataset.id
+    if (e.target.innerHTML == '+'){
+      this.changeExp(parseInt(this.statTargets[buttonId].value), true)
+      this.statTargets[buttonId].value++
+    } else {
+      this.changeExp(parseInt(this.statTargets[buttonId].value), false)
+      this.statTargets[buttonId].value--
+    }
+    this.checkStatLevels()
+  }
+
+  changeExp(statValue, isAdding){
+    switch (statValue) {
       case 20:
-        this.expTarget.value = expPoints - 20
+        this.expTarget.value += 20
         break
       case 19:
+        isAdding ? this.expTarget.value -= 20 : this.expTarget.value = parseInt(this.expTarget.value) + 15
+        break
       case 18:
       case 17:
-        this.expTarget.value = expPoints - 15
+        isAdding ? this.expTarget.value -= 15 : this.expTarget.value = parseInt(this.expTarget.value) + 15
         break
       case 16:
+        isAdding ? this.expTarget.value -= 15 : this.expTarget.value = parseInt(this.expTarget.value) + 10
+        break
       case 15:
       case 14:
-        this.expTarget.value = expPoints - 10
+        isAdding ? this.expTarget.value -= 10 : this.expTarget.value = parseInt(this.expTarget.value) + 10
+        break
+      case 13:
+        isAdding ? this.expTarget.value -= 10 : this.expTarget.value = parseInt(this.expTarget.value) + 5
         break
       default:
-        this.expTarget.value = expPoints - 5
+        isAdding ? this.expTarget.value -= 5 : this.expTarget.value = parseInt(this.expTarget.value) + 5
         break
-    }
-  }
-
-  changeValue(button, target){
-    const id = button.dataset.id
-    if (button.innerHTML == '-'){
-      target.value--
-      this.addExp(target)
-      this.plusTargets[id].disabled = false
-      if (target.value == 7){
-        button.disabled = true
-      }
-    }else{
-      target.value++
-      this.cutExp(target)
-      this.minusTargets[id].disabled = false
-      if (target.value == 20){
-        button.disabled = true
-      }
-    }
-  }
-
-  changeStat(){
-    const eventTarget = event.target
-    switch (eventTarget.dataset.id) {
-      case '0':
-        this.changeValue(eventTarget, this.brawnTarget)
-        break
-      case '1':
-        this.changeValue(eventTarget, this.deterTarget)
-        break
-      case '2':
-        this.changeValue(eventTarget, this.intelTarget)
-        break
-      case '3':
-        this.changeValue(eventTarget, this.perceTarget)
-        break
-      case '4':
-        this.changeValue(eventTarget, this.dexteTarget)
-        break
-      case '5':
-        this.changeValue(eventTarget, this.agiliTarget)
-        break
-      default:
-        console.log('Error')
     }
   }
 }
