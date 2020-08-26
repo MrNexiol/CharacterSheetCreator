832c7259abb7b07452ee61bfd501e0b43970b84f
Tomasz Kopycinski <mrnexiol@gmail.com>
2020-08-26 21:04:39 +0200

    Added changing values of skills together with exp reduction/addition

--- app/javascript/controllers/new_character_sheet_controller.js
+++ app/javascript/controllers/new_character_sheet_controller.js
@@ -1,16 +1,31 @@
-import { Controller } from "stimulus"
+import {Controller} from "stimulus"
 
 export default class extends Controller {
-  static targets = ['exp', 'stat', 'plus', 'minus']
+  static targets = ['exp', 'stat', 'skill', 'plus', 'minus', 'plusSkill', 'minusSkill']
 
   connect(){
     this.checkStatLevels()
+    this.checkSkillLevels()
   }
 
   checkStatLevels(){
     for (let i = 0; i < this.statTargets.length; i++){
       const canUpgrade = this.isStatUpgradeable(parseInt(this.statTargets[i].value));
       this.plusTargets[i].disabled = !canUpgrade;
+    }
+  }
+
+  checkSkillLevels(){
+    for (let i = 0; i < this.skillTargets.length; i++){
+      const parentId = this.skillTargets[i].dataset.parent
+      const skillId = this.skillTargets[i].dataset.id
+      if (parseInt(this.skillTargets[i].value) < parseInt(this.statTargets[parentId].value) && parseInt(this.skillTargets[i].value) < 25){
+        this.getSkillButtonById(skillId).disabled = parseInt(this.expTarget.value) <= 0;
+      }else if (parseInt(this.skillTargets[i].value) >= parseInt(this.statTargets[parentId].value) && parseInt(this.skillTargets[i].value) < 25) {
+        this.getSkillButtonById(skillId).disabled = parseInt(this.expTarget.value) < (parseInt(this.skillTargets[i].value) - parseInt(this.statTargets[parentId].value))+2;
+      } else {
+        this.getSkillButtonById(skillId).disabled = true
+      }
     }
   }
 
@@ -34,22 +49,58 @@ export default class extends Controller {
     }
   }
 
+  getSkillButtonById(id){
+    return this.plusSkillTargets.find(obj => {
+      return obj.dataset.id == id
+    })
+  }
+
+  getSkillById(id){
+    return this.skillTargets.find(obj => {
+      return obj.dataset.id == id
+    })
+  }
+
+  changeSkill(e){
+    const buttonId = e.target.dataset.id
+    const skill = this.getSkillById(buttonId)
+    if (e.target.innerHTML == '+') {
+      this.changeExpFromSkill(skill, true)
+      skill.value++
+    }else {
+      this.changeExpFromSkill(skill, false)
+      skill.value--
+    }
+    this.checkStatLevels()
+    this.checkSkillLevels()
+  }
+
   changeStat(e){
     const buttonId = e.target.dataset.id
     if (e.target.innerHTML == '+'){
-      this.changeExp(parseInt(this.statTargets[buttonId].value), true)
+      this.changeExpFromStat(parseInt(this.statTargets[buttonId].value), true)
       this.statTargets[buttonId].value++
     } else {
-      this.changeExp(parseInt(this.statTargets[buttonId].value), false)
+      this.changeExpFromStat(parseInt(this.statTargets[buttonId].value), false)
       this.statTargets[buttonId].value--
     }
     this.checkStatLevels()
+    this.checkSkillLevels()
   }
 
-  changeExp(statValue, isAdding){
+  changeExpFromSkill(skill, isAdding){
+    if (parseInt(skill.value) < parseInt(this.statTargets[skill.dataset.parent].value)) {
+      isAdding ? this.expTarget.value = parseInt(this.expTarget.value) - 1 : this.expTarget.value = parseInt(this.expTarget.value) + 1
+    }else {
+      isAdding ? this.expTarget.value = parseInt(this.expTarget.value) - (parseInt(skill.value) - parseInt(this.statTargets[skill.dataset.parent].value) + 2) :
+          this.expTarget.value = parseInt(this.expTarget.value) + (parseInt(skill.value) - parseInt(this.statTargets[skill.dataset.parent].value) + 1)
+    }
+  }
+
+  changeExpFromStat(statValue, isAdding){
     switch (statValue) {
       case 20:
-        this.expTarget.value += 20
+        this.expTarget.value = parseInt(this.expTarget.value) + 20
         break
       case 19:
         isAdding ? this.expTarget.value -= 20 : this.expTarget.value = parseInt(this.expTarget.value) + 15
