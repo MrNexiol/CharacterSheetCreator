2a00074ca5e14b88d9f8c2ad7b4b9d7c40c3fedb
Tomasz Kopycinski <mrnexiol@gmail.com>
2020-08-27 09:11:38 +0200

    Modified the way program disables + and - buttons for skills, added base skill value to js controller and fixed seeds

--- app/javascript/controllers/new_character_sheet_controller.js
+++ app/javascript/controllers/new_character_sheet_controller.js
@@ -4,29 +4,12 @@ export default class extends Controller {
   static targets = ['exp', 'stat', 'skill', 'plus', 'minus', 'plusSkill', 'minusSkill']
 
   connect(){
+    this.checkPoints()
+  }
+
+  checkPoints(){
     this.checkStatLevels()
     this.checkSkillLevels()
-  }
-
-  checkStatLevels(){
-    for (let i = 0; i < this.statTargets.length; i++){
-      const canUpgrade = this.isStatUpgradeable(parseInt(this.statTargets[i].value));
-      this.plusTargets[i].disabled = !canUpgrade;
-    }
-  }
-
-  checkSkillLevels(){
-    for (let i = 0; i < this.skillTargets.length; i++){
-      const parentId = this.skillTargets[i].dataset.parent
-      const skillId = this.skillTargets[i].dataset.id
-      if (parseInt(this.skillTargets[i].value) < parseInt(this.statTargets[parentId].value) && parseInt(this.skillTargets[i].value) < 25){
-        this.getSkillButtonById(skillId).disabled = parseInt(this.expTarget.value) <= 0;
-      }else if (parseInt(this.skillTargets[i].value) >= parseInt(this.statTargets[parentId].value) && parseInt(this.skillTargets[i].value) < 25) {
-        this.getSkillButtonById(skillId).disabled = parseInt(this.expTarget.value) < (parseInt(this.skillTargets[i].value) - parseInt(this.statTargets[parentId].value))+2;
-      } else {
-        this.getSkillButtonById(skillId).disabled = true
-      }
-    }
   }
 
   isStatUpgradeable(testValue){
@@ -49,10 +32,16 @@ export default class extends Controller {
     }
   }
 
-  getSkillButtonById(id){
-    return this.plusSkillTargets.find(obj => {
-      return obj.dataset.id == id
-    })
+  getSkillButtonById(id, isPlus){
+    if (isPlus){
+      return this.plusSkillTargets.find(obj => {
+        return obj.dataset.id == id
+      })
+    }else{
+      return this.minusSkillTargets.find(obj => {
+        return obj.dataset.id == id
+      })
+    }
   }
 
   getSkillById(id){
@@ -71,8 +60,7 @@ export default class extends Controller {
       this.changeExpFromSkill(skill, false)
       skill.value--
     }
-    this.checkStatLevels()
-    this.checkSkillLevels()
+    this.checkPoints()
   }
 
   changeStat(e){
@@ -84,8 +72,7 @@ export default class extends Controller {
       this.changeExpFromStat(parseInt(this.statTargets[buttonId].value), false)
       this.statTargets[buttonId].value--
     }
-    this.checkStatLevels()
-    this.checkSkillLevels()
+    this.checkPoints()
   }
 
   changeExpFromSkill(skill, isAdding){
@@ -122,6 +109,30 @@ export default class extends Controller {
       default:
         isAdding ? this.expTarget.value -= 5 : this.expTarget.value = parseInt(this.expTarget.value) + 5
         break
+    }
+  }
+
+  checkStatLevels(){
+    for (let i = 0; i < this.statTargets.length; i++){
+      const canUpgrade = this.isStatUpgradeable(parseInt(this.statTargets[i].value));
+      this.plusTargets[i].disabled = !canUpgrade;
+      this.minusTargets[i].disabled = parseInt(this.statTargets[i].value) < 1;
+    }
+  }
+
+  checkSkillLevels(){
+    for (let i = 0; i < this.skillTargets.length; i++){
+      const parentId = this.skillTargets[i].dataset.parent
+      const skillId = this.skillTargets[i].dataset.id
+      this.getSkillButtonById(skillId, false).disabled = parseInt(this.skillTargets[i].value) <= parseInt(this.skillTargets[i].dataset.base);
+
+      if (parseInt(this.skillTargets[i].value) < parseInt(this.statTargets[parentId].value) && parseInt(this.skillTargets[i].value) < 25){
+        this.getSkillButtonById(skillId, true).disabled = parseInt(this.expTarget.value) <= 0;
+      }else if (parseInt(this.skillTargets[i].value) >= parseInt(this.statTargets[parentId].value) && parseInt(this.skillTargets[i].value) < 25) {
+        this.getSkillButtonById(skillId, true).disabled = parseInt(this.expTarget.value) < (parseInt(this.skillTargets[i].value) - parseInt(this.statTargets[parentId].value))+2;
+      } else {
+        this.getSkillButtonById(skillId, true).disabled = true
+      }
     }
   }
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@
 
 User.create(username: 'Tomaszowy', email: 'admin@example.com', password: '123456')
 
-Skill.create(name: 'Obserwacja', start_value: 1, parent_stat: Skill::STAT_NAMES[3])
-Skill.create(name: 'Manipulacja', start_value: 2, parent_stat: Skill::STAT_NAMES[3])
-Skill.create(name: 'Komunikacja', start_value: 3, parent_stat: Skill::STAT_NAMES[3])
-Skill.create(name: 'Siła', start_value: 4, parent_stat: Skill::STAT_NAMES[0])
-Skill.create(name: 'Odporność na ból', start_value: 5, parent_stat: Skill::STAT_NAMES[1])
-Skill.create(name: 'Kanalarstwo', start_value: 6, parent_stat: Skill::STAT_NAMES[3])
+Skill.create(name: 'Obserwacja', start_value: 8, parent_stat: Skill::STAT_NAMES[3])
+Skill.create(name: 'Manipulacja', start_value: 8, parent_stat: Skill::STAT_NAMES[3])
+Skill.create(name: 'Komunikacja', start_value: 8, parent_stat: Skill::STAT_NAMES[3])
+Skill.create(name: 'Siła', start_value: 8, parent_stat: Skill::STAT_NAMES[0])
+Skill.create(name: 'Odporność na ból', start_value: 8, parent_stat: Skill::STAT_NAMES[1])
+Skill.create(name: 'Kanalarstwo', start_value: 0, parent_stat: Skill::STAT_NAMES[3])
